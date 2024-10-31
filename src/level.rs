905820ffdcb5cbeb887a162ee562b30fc204dc20
stinkytoe <stinkytoe42@yahoo.com>
2024-10-31 13:48:42 -0500

    removing old commented code

--- src/level.rs
+++ src/level.rs
@@ -10,35 +10,3 @@ pub struct Level {
     pub handle: Handle<ldtk_asset::Level>,
     pub config: Handle<ProjectConfig>,
 }
-
-//impl Default for Level {
-//    fn default() -> Self {
-//        Self {
-//            load_pattern: LoadPattern::All,
-//            handle: Handle::default(),
-//        }
-//    }
-//}
-
-//impl crate::component_traits::LdtkComponent for Level {
-//    type Asset = ldtk_asset::Level;
-//
-//    fn new(load_pattern: LoadPattern, handle: Handle<Self::Asset>) -> Self {
-//        Self {
-//            load_pattern,
-//            handle,
-//        }
-//    }
-//
-//    fn get_handle(&self) -> &Handle<Self::Asset> {
-//        &self.handle
-//    }
-//
-//    fn get_load_pattern(&self) -> &LoadPattern {
-//        &self.load_pattern
-//    }
-//}
-//
-//impl HasChildren for Level {
-//    type Child = Layer;
-//}
