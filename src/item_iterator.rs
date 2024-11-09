4c86559c6336e771f7ed4533f1646686cc50ea38
stinkytoe <stinkytoe42@yahoo.com>
2024-11-09 15:59:58 -0600

    iterators are hard

--- src/item_iterator.rs
+++ src/item_iterator.rs
@@ -6,25 +6,23 @@ use bevy_ldtk_asset::ldtk_asset_trait::LdtkAsset;
 
 use crate::item::{LdtkItem, LdtkItemTrait};
 
-pub trait LdtkItemIterator<Asset>
+pub trait LdtkItemIterator<'a, Asset>
 where
-    Self: Iterator + Sized,
-    Self::Item: LdtkItemTrait<Asset>,
+    Self: Iterator<Item = LdtkItem<'a, Asset>> + Sized,
     Asset: LdtkAsset + Sized + std::fmt::Debug,
 {
     fn find_iid(mut self, iid: Iid) -> Option<impl LdtkItemTrait<Asset>> {
         self.find(|item| item.get_iid() == iid)
     }
 
-    fn find_ecs_entity(mut self, ecs_entity: EcsEntity) -> Option<impl LdtkItemTrait<Asset>> {
+    fn find_ecs_entity(mut self, ecs_entity: EcsEntity) -> Option<LdtkItem<'a, Asset>> {
         self.find(|item| item.get_ecs_entity() == ecs_entity)
     }
 }
 
-impl<Asset, Iter> LdtkItemIterator<Asset> for Iter
+impl<'a, Asset, Iter> LdtkItemIterator<'a, Asset> for Iter
 where
-    Iter: Iterator + Sized,
-    Iter::Item: LdtkItemTrait<Asset>,
+    Iter: Iterator<Item = LdtkItem<'a, Asset>> + Sized,
     Asset: LdtkAsset + Sized + std::fmt::Debug,
 {
 }
