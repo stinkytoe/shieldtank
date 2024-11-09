5f1b0ba2fdee389977592756b407dbd32bf9f9fa
stinkytoe <stinkytoe42@yahoo.com>
2024-11-09 13:58:24 -0600

    filling out methods for Level, Layer

--- src/query.rs
+++ src/query.rs
@@ -9,8 +9,8 @@ use bevy_ldtk_asset::project::Project as ProjectAsset;
 use bevy_ldtk_asset::world::World as WorldAsset;
 
 use crate::entity::{Entity, EntityItem};
-use crate::layer::Layer;
-use crate::level::Level;
+use crate::layer::{Layer, LayerItem};
+use crate::level::{Level, LevelItem};
 use crate::project::Project;
 use crate::world::World;
 //use crate::layer::{LayerData, LayerItem};
@@ -24,25 +24,43 @@ pub struct LdtkQuery<'w, 's> {
     // For each component type
     pub(crate) _project_assets: Res<'w, Assets<ProjectAsset>>,
     pub(crate) _projects_query: Query<'w, 's, (EcsEntity, &'static Project)>,
-    pub(crate) _level_assets: Res<'w, Assets<LevelAsset>>,
-    pub(crate) _levels_query: Query<'w, 's, (EcsEntity, &'static Level)>,
     pub(crate) _world_assets: Res<'w, Assets<WorldAsset>>,
     pub(crate) _worlds_query: Query<'w, 's, (EcsEntity, &'static World)>,
-    pub(crate) _layer_assets: Res<'w, Assets<LayerAsset>>,
-    pub(crate) _layers_query: Query<'w, 's, (EcsEntity, &'static Layer)>,
+    pub(crate) level_assets: Res<'w, Assets<LevelAsset>>,
+    pub(crate) levels_query: Query<'w, 's, (EcsEntity, &'static Level)>,
+    pub(crate) layer_assets: Res<'w, Assets<LayerAsset>>,
+    pub(crate) layers_query: Query<'w, 's, (EcsEntity, &'static Layer)>,
     pub(crate) entity_assets: Res<'w, Assets<EntityAsset>>,
     pub(crate) entities_query: Query<'w, 's, (EcsEntity, &'static Entity)>,
 }
 
 impl LdtkQuery<'_, '_> {
-    //pub fn levels(&self) -> impl Iterator<Item = LevelItem> {
-    //    LevelItem::make_level_iterator(self)
-    //}
-    //
-    //pub fn layers(&self) -> impl Iterator<Item = LayerItem> {
-    //    LayerItem::make_layer_iterator(self)
-    //}
-    //
+    pub fn levels(&self) -> impl Iterator<Item = LevelItem> {
+        self.levels_query
+            .iter()
+            .filter_map(|(ecs_entity, component)| {
+                Some((ecs_entity, self.level_assets.get(component.handle.id())?))
+            })
+            .map(|(ecs_entity, asset)| LevelItem {
+                asset,
+                ecs_entity,
+                query: self,
+            })
+    }
+
+    pub fn layers(&self) -> impl Iterator<Item = LayerItem> {
+        self.layers_query
+            .iter()
+            .filter_map(|(ecs_entity, component)| {
+                Some((ecs_entity, self.layer_assets.get(component.handle.id())?))
+            })
+            .map(|(ecs_entity, asset)| LayerItem {
+                asset,
+                ecs_entity,
+                query: self,
+            })
+    }
+
     pub fn entities(&self) -> impl Iterator<Item = EntityItem> {
         //EntityItem::make_entity_iterator(self)
         self.entities_query
