3d3a25419612634bf6d863df71a9544b46545782
stinkytoe <stinkytoe42@yahoo.com>
2024-11-09 13:36:54 -0600

    added .find_ecs_entity(..) method

--- src/item_iterator.rs
+++ src/item_iterator.rs
@@ -1,3 +1,4 @@
+use bevy_ecs::entity::Entity as EcsEntity;
 use bevy_ldtk_asset::iid::Iid;
 use bevy_ldtk_asset::ldtk_asset_trait::LdtkAsset;
 
@@ -11,6 +12,10 @@ where
 {
     fn find_iid(mut self, iid: Iid) -> Option<impl LdtkItemTrait<Asset>> {
         self.find(|item| item.get_iid() == iid)
+    }
+
+    fn find_ecs_entity(mut self, ecs_entity: EcsEntity) -> Option<impl LdtkItemTrait<Asset>> {
+        self.find(|item| item.get_ecs_entity() == ecs_entity)
     }
 }
 
