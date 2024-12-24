99e60dd24c2b6accb79a168304f976aaccb24131
stinkytoe <stinkytoe42@yahoo.com>
2024-12-24 14:16:24 -0600

    improvement to location members

--- src/world.rs
+++ src/world.rs
@@ -43,16 +43,19 @@ impl WorldItem<'_> {
 }
 
 impl WorldItem<'_> {
-    pub fn int_grid_value_at_global_location(&self, global_location: Vec2) -> Option<IntGridValue> {
+    pub fn int_grid_value_at_world_location(&self, world_location: Vec2) -> Option<IntGridValue> {
+        let self_location = self.get_transform()?.translation.truncate();
+        let global_location = self_location + world_location;
+
         let mut levels: Vec<_> = self
             .get_levels()
             .filter_global_location(global_location)
             .collect();
 
-        // unwrap is OK here because the above collect wouldn't have yielded anything that didn't
-        // have a global_transform component.
         #[allow(clippy::unwrap_used)]
         levels.sort_by(|a, b| {
+            // unwrap is OK here because the above collect wouldn't have yielded anything that didn't
+            // have a global_transform component.
             let a_z = a.get_global_transform().unwrap().translation().z;
             let b_z = b.get_global_transform().unwrap().translation().z;
             // intentionally reversed, so we will search nearest to farthest when looking down in
@@ -64,6 +67,28 @@ impl WorldItem<'_> {
             .iter()
             .find_map(|level_item| level_item.int_grid_value_at_global_location(global_location))
     }
+
+    // pub fn int_grid_value_at_global_location(&self, global_location: Vec2) -> Option<IntGridValue> {
+    //     let mut levels: Vec<_> = self
+    //         .get_levels()
+    //         .filter_global_location(global_location)
+    //         .collect();
+    //
+    //     #[allow(clippy::unwrap_used)]
+    //     levels.sort_by(|a, b| {
+    //         // unwrap is OK here because the above collect wouldn't have yielded anything that didn't
+    //         // have a global_transform component.
+    //         let a_z = a.get_global_transform().unwrap().translation().z;
+    //         let b_z = b.get_global_transform().unwrap().translation().z;
+    //         // intentionally reversed, so we will search nearest to farthest when looking down in
+    //         // the world from above.
+    //         b_z.partial_cmp(&a_z).unwrap()
+    //     });
+    //
+    //     levels
+    //         .iter()
+    //         .find_map(|level_item| level_item.int_grid_value_at_global_location(global_location))
+    // }
 }
 
 pub struct WorldPlugin;
