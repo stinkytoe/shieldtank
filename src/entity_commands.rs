5577bab1a184913816ce7a4cf0a3a111411eb7ca
stinkytoe <stinkytoe42@yahoo.com>
2025-01-02 15:44:55 -0600

    changed set_global_location to set_world_location

--- src/entity_commands.rs
+++ src/entity_commands.rs
@@ -7,16 +7,43 @@ use crate::item_commands::LdtkItemCommands;
 pub type EntityCommands<'a> = LdtkItemCommands<'a, EntityAsset>;
 
 impl EntityCommands<'_> {
-    pub fn set_global_location(&mut self, global_location: Vec2) -> &mut Self {
+    pub fn set_world_location(&mut self, new_world_location: Vec2) -> &mut Self {
         let ecs_entity = self.item.get_ecs_entity();
-        if let (Some(current_global_location), Ok(current_transform)) = (
-            self.item.get_global_location(),
+
+        if let (Some(world_location), Ok(current_transform)) = (
+            self.item.get_world_local_location(),
             self.item.query.transform_query.get(ecs_entity),
         ) {
-            let offset = global_location - current_global_location;
+            let new_location = new_world_location - world_location;
             let new_transform = current_transform
-                .with_translation(current_transform.translation + offset.extend(0.0));
+                .with_translation(current_transform.translation + new_location.extend(0.0));
+            self.commands.entity(ecs_entity).insert(new_transform);
+        }
 
+        self
+    }
+
+    pub fn set_level_location(&mut self, new_level_location: Vec2) -> &mut Self {
+        let ecs_entity = self.item.get_ecs_entity();
+
+        if let (Some(level_location), Ok(current_transform)) = (
+            self.item.get_level_local_location(),
+            self.item.query.transform_query.get(ecs_entity),
+        ) {
+            let new_location = new_level_location - level_location;
+            let new_transform = current_transform
+                .with_translation(current_transform.translation + new_location.extend(0.0));
+            self.commands.entity(ecs_entity).insert(new_transform);
+        }
+
+        self
+    }
+
+    pub fn set_layer_location(&mut self, new_layer_location: Vec2) -> &mut Self {
+        let ecs_entity = self.item.get_ecs_entity();
+
+        if let Ok(current_transform) = self.item.query.transform_query.get(ecs_entity) {
+            let new_transform = current_transform.with_translation(new_layer_location.extend(0.0));
             self.commands.entity(ecs_entity).insert(new_transform);
         }
 
