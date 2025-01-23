709b2f12bca903d9b2a0482a36e84beb58f02d2c
stinkytoe <stinkytoe42@yahoo.com>
2025-01-22 23:06:16 -0600

    code cleanup

--- src/item/iter/unique_identifier.rs
+++ src/item/iter/unique_identifier.rs
@@ -1,5 +1,15 @@
 use bevy_ecs::query::QueryData;
 use bevy_ldtk_asset::ldtk_asset_trait::LdtkAsset;
+use bevy_ldtk_asset::level::Level as LevelAsset;
+use bevy_ldtk_asset::project::Project as ProjectAsset;
+use bevy_ldtk_asset::world::World as WorldAsset;
+
+use crate::component::level::LevelComponentQueryData;
+use crate::component::project::ProjectComponentQueryData;
+use crate::component::world::WorldComponentQueryData;
+use crate::item::level::LevelItem;
+use crate::item::project::ProjectItem;
+use crate::item::world::WorldItem;
 
 use super::Item;
 
@@ -15,25 +25,16 @@ where
     }
 }
 
-use crate::component::project::ProjectComponentQueryData;
-use crate::item::project::ProjectItem;
-use bevy_ldtk_asset::project::Project as ProjectAsset;
 impl<'w, 's: 'w, I: Iterator<Item = ProjectItem<'w, 's>>>
     ItemIteratorUniqueIdentifierExt<'w, 's, ProjectAsset, ProjectComponentQueryData<'w>> for I
 {
 }
 
-use crate::component::world::WorldComponentQueryData;
-use crate::item::world::WorldItem;
-use bevy_ldtk_asset::world::World as WorldAsset;
 impl<'w, 's: 'w, I: Iterator<Item = WorldItem<'w, 's>>>
     ItemIteratorUniqueIdentifierExt<'w, 's, WorldAsset, WorldComponentQueryData<'w>> for I
 {
 }
 
-use crate::component::level::LevelComponentQueryData;
-use crate::item::level::LevelItem;
-use bevy_ldtk_asset::level::Level as LevelAsset;
 impl<'w, 's: 'w, I: Iterator<Item = LevelItem<'w, 's>>>
     ItemIteratorUniqueIdentifierExt<'w, 's, LevelAsset, LevelComponentQueryData<'w>> for I
 {
