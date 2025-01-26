5158c7218f2035b2c654fa9d94fc747c3d4f2bd3
stinkytoe <stinkytoe42@yahoo.com>
2025-01-25 20:12:52 -0600

    added animate_water and message board

--- examples/axe_man_adventure/src/main.rs
+++ examples/axe_man_adventure/src/main.rs
@@ -1,10 +1,38 @@
+use std::time::Duration;
+
+use bevy::color::palettes::tailwind::GRAY_500;
 use bevy::prelude::*;
 use bevy::window::WindowMode;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use shieldtank::commands::ShieldtankCommands;
 use shieldtank::component::project::ProjectComponent;
+use shieldtank::item::iter::recurrent_identifier::ItemRecurrentIdentifierIteratorExt;
 use shieldtank::plugin::ShieldtankPlugins;
 use shieldtank::project_config::ProjectConfig;
 use shieldtank::query::ShieldtankQuery;
+
+const RESOLUTION: Vec2 = Vec2::new(1280.0, 960.0);
+const GLOBAL_FRAME_TIME: f32 = 1.0 / 3.75;
+
+#[derive(Event)]
+struct GlobalAnimationEvent {
+    frame: usize,
+}
+
+#[derive(Debug, Resource, Reflect)]
+pub(crate) struct AnimationTimer {
+    pub(crate) timer: Timer,
+    pub(crate) frame: usize,
+}
+
+impl AnimationTimer {
+    pub fn new(frame_time: f32) -> Self {
+        Self {
+            timer: Timer::new(Duration::from_secs_f32(frame_time), TimerMode::Repeating),
+            frame: 0,
+        }
+    }
+}
 
 fn main() {
     let log_plugin_settings = bevy::log::LogPlugin {
@@ -18,8 +46,6 @@ fn main() {
             .into(),
         ..default()
     };
-
-    const RESOLUTION: Vec2 = Vec2::new(1280.0, 960.0);
 
     let window_plugin_settings = WindowPlugin {
         primary_window: Some(Window {
@@ -41,23 +67,101 @@ fn main() {
         ShieldtankPlugins,
         WorldInspectorPlugin::default(),
     ))
+    .add_observer(animate_water)
     .add_systems(Startup, startup)
-    .add_systems(Update, update);
+    .add_systems(Update, (update_global_animation_timer,))
+    .insert_resource(AnimationTimer::new(GLOBAL_FRAME_TIME));
 
     app.run();
 }
 
 fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
     debug!("Spawning project...");
+    commands.spawn((
+        Camera2d,
+        Transform::from_xyz(0.0, -128.0, 0.0).with_scale(Vec2::splat(0.4).extend(1.0)),
+    ));
+
     commands.spawn(ProjectComponent {
         handle: asset_server.load("ldtk/axe_man_adventure.ldtk"),
         config: asset_server.add(ProjectConfig::default()),
     });
 
     commands.spawn((
-        Camera2d,
-        Transform::from_xyz(0.0, -128.0, 0.0).with_scale(Vec2::splat(0.4).extend(1.0)),
+        Name::new("MessageBoard"),
+        Text::new("The Axe Man begins his adventure!"),
+        TextFont {
+            font: asset_server.load("fonts/Primitive.ttf"),
+            font_size: 50.0,
+            ..Default::default()
+        },
+        TextColor(GRAY_500.into()),
+        TextLayout::new_with_justify(JustifyText::Center),
+        Node {
+            position_type: PositionType::Absolute,
+            bottom: Val::Px(40.0),
+            left: Val::Px(5.0),
+            right: Val::Px(5.0),
+            ..default()
+        },
+        // MessageBoard,
     ));
 }
 
-fn update(_shieldtank_query: ShieldtankQuery) {}
+fn update_global_animation_timer(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut animation_timer: ResMut<AnimationTimer>,
+) {
+    animation_timer.timer.tick(time.delta());
+
+    if animation_timer.timer.just_finished() {
+        animation_timer.frame += 1;
+        animation_timer.frame %= 4;
+
+        commands.trigger(GlobalAnimationEvent {
+            frame: animation_timer.frame,
+        });
+
+        // query
+        //     .iter()
+        //     .for_each(|(entity, animation_state)| match animation_state {
+        //         AnimationState::Idle | AnimationState::Moving | AnimationState::Dead => {
+        //             commands.entity(entity).trigger(EntityAnimationEvent);
+        //         }
+        //         _ => {}
+        //     });
+    }
+}
+
+fn animate_water(
+    trigger: Trigger<GlobalAnimationEvent>,
+    mut shieldtank_commands: ShieldtankCommands,
+    shieldtank_query: ShieldtankQuery,
+) {
+    const LAYERS_TO_ANIMATE: &[&str] = &["Water1", "Water2", "Water3", "Water4"];
+
+    let GlobalAnimationEvent { frame } = *trigger.event();
+
+    for level in shieldtank_query.iter_levels() {
+        for (index, layer_identifier) in LAYERS_TO_ANIMATE.iter().enumerate() {
+            let Some(layer) = level
+                .iter_layers()
+                .filter_identifier(layer_identifier)
+                .next()
+            else {
+                return;
+            };
+
+            let visibility = if frame == index {
+                Visibility::Visible
+            } else {
+                Visibility::Hidden
+            };
+
+            shieldtank_commands
+                .layer(&layer)
+                .insert_visibility_component(visibility);
+        }
+    }
+}
