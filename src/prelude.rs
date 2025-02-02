cdd8c10a41c88cccdc94bad9e6fed5de8312ac53
stinkytoe <stinkytoe42@yahoo.com>
2025-02-02 15:10:48 -0600

    added LoadPattern to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -10,6 +10,7 @@ pub use crate::item::entity::iter::LocationInRegionIteratorExt as _;
 pub use crate::item::entity::iter::WorldLocationInRegionIteratorExt as _;
 pub use crate::item::iter::recurrent_identifier::ItemRecurrentIdentifierIteratorExt as _;
 pub use crate::item::iter::ItemIteratorExt as _;
+pub use crate::load_pattern::LoadPattern;
 pub use crate::plugin::ShieldtankPlugins;
 pub use crate::project_config::ProjectConfig;
 pub use crate::query::ShieldtankQuery;
