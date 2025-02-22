f11a6cea76e9dd990a836be2cf2928540d968469
stinkytoe <stinkytoe42@yahoo.com>
2025-02-22 12:40:05 -0600

    for levels, overriden transform now obeys world layout type

--- src/item/level/systems.rs
+++ src/item/level/systems.rs
@@ -1,4 +1,6 @@
+use bevy_ldtk_asset::world::WorldLayout;
 use bevy_math::Vec2;
+use bevy_transform::components::Transform;
 
 use crate::commands::ShieldtankCommands;
 use crate::item::iter::ItemIteratorExt as _;
@@ -37,12 +39,29 @@ pub(crate) fn level_override_transform_system(
             let config = item.get_config();
 
             if config.levels_override_transform() {
-                let asset = item.get_asset();
-                let location = Vec2::new(1.0, -1.0) * asset.location.as_vec2();
-                let z = asset.world_depth as f32 * config.level_separation();
-                let transform = item.get_transform().with_translation(location.extend(z));
+                let world = item.get_world();
 
-                shieldtank_commands.level(&item).insert(transform);
+                match world {
+                    Some(world)
+                        if matches!(world.get_asset().world_layout, WorldLayout::Free)
+                            || matches!(
+                                world.get_asset().world_layout,
+                                WorldLayout::GridVania(_)
+                            ) =>
+                    {
+                        let asset = item.get_asset();
+                        let location = Vec2::new(1.0, -1.0) * asset.location.as_vec2();
+                        let z = asset.world_depth as f32 * config.level_separation();
+                        let transform = item.get_transform().with_translation(location.extend(z));
+
+                        shieldtank_commands.level(&item).insert(transform);
+                    }
+                    _ => {
+                        shieldtank_commands
+                            .level(&item)
+                            .insert(Transform::default());
+                    }
+                }
             }
         });
 }
