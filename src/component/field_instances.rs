b7fbfa2febe140c48dcd9cceff9765c3f695530b
stinkytoe <stinkytoe42@yahoo.com>
2025-04-22 21:41:09 -0500

    get_array_string now returns the Vec<String>

--- src/component/field_instances.rs
+++ src/component/field_instances.rs
@@ -28,12 +28,10 @@ impl FieldInstances {
             .map(Tile::new)
     }
 
-    pub fn get_array_string(&self, identifier: &str, index: usize) -> Option<&str> {
+    pub fn get_array_string(&self, identifier: &str) -> Option<&Vec<String>> {
         self.field_instances
             .get(identifier)
             .and_then(|strings| strings.get_array_string())
-            .and_then(|strings| strings.get(index))
-            .map(|string| string.as_str())
     }
 }
 pub struct FieldInstancesPlugin;
