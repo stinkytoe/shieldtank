44d1dc60b1f7a700f53ed733d6b5ce4e56d5479d
stinkytoe <stinkytoe42@yahoo.com>
2025-04-26 12:25:37 -0500

    removed Deref from Tags, gave it a contains(..) method

--- src/component/tags.rs
+++ src/component/tags.rs
@@ -1,11 +1,9 @@
 use bevy_app::Plugin;
-use bevy_derive::Deref;
 use bevy_ecs::component::Component;
 use bevy_reflect::Reflect;
 
-#[derive(Debug, Deref, Component, Reflect)]
+#[derive(Debug, Component, Reflect)]
 pub struct Tags {
-    #[deref]
     tags: Vec<String>,
 }
 
@@ -13,6 +11,10 @@ impl Tags {
     pub fn new<T: AsRef<str> + std::fmt::Display>(tags: &[T]) -> Self {
         let tags = tags.iter().map(|tag| tag.to_string()).collect();
         Self { tags }
+    }
+
+    pub fn contains(&self, tag: &str) -> bool {
+        self.tags.iter().any(|inner_tag| *inner_tag == tag)
     }
 }
 
