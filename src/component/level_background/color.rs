413618bc6df5ee382dd432ee1b4c43aef8358945
stinkytoe <stinkytoe42@yahoo.com>
2025-05-04 10:18:33 -0500

    fixed bug where multiple background color children were spawned

--- src/component/level_background/color.rs
+++ src/component/level_background/color.rs
@@ -2,7 +2,9 @@ use bevy_asset::Assets;
 use bevy_color::Color;
 use bevy_ecs::component::Component;
 use bevy_ecs::entity::Entity;
-use bevy_ecs::query::Changed;
+use bevy_ecs::hierarchy::Children;
+use bevy_ecs::name::Name;
+use bevy_ecs::query::{Changed, With};
 use bevy_ecs::removal_detection::RemovedComponents;
 use bevy_ecs::system::{Commands, Query, ResMut};
 use bevy_log::debug;
@@ -19,21 +21,40 @@ pub struct LevelBackgroundColor {
     pub size: Vec2,
 }
 
-pub fn level_background_color_system(
-    query: Query<(Entity, &LevelBackgroundColor), Changed<LevelBackgroundColor>>,
+#[derive(Component)]
+pub(crate) struct LdtkLevelBackgroundColorMesh;
+
+pub(crate) fn level_background_color_system(
+    query: Query<(Entity, &LevelBackgroundColor, Option<&Children>), Changed<LevelBackgroundColor>>,
+    background_children_query: Query<Entity, With<LdtkLevelBackgroundColorMesh>>,
     mut removed: RemovedComponents<LevelBackgroundColor>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    query.iter().for_each(|(entity, component)| {
+    query.iter().for_each(|(entity, component, children)| {
+        // remove old meshes, if any
+        children.into_iter().for_each(|children| {
+            children
+                .into_iter()
+                .copied()
+                .filter_map(|child| background_children_query.get(child).ok())
+                .for_each(|child| {
+                    commands.entity(child).despawn();
+                });
+        });
+
         let width = component.size.x;
         let height = -component.size.y;
 
+        let name = Name::new("background color mesh");
         let mesh_2d = Mesh2d(meshes.add(Rectangle::new(width, height)));
         let material = MeshMaterial2d(materials.add(component.color));
         let transform = Transform::from_xyz(width / 2.0, height / 2.0, 0.0);
-        let child = commands.spawn((mesh_2d, material, transform)).id();
+        let background_color_mesh = LdtkLevelBackgroundColorMesh;
+        let child = commands
+            .spawn((name, mesh_2d, material, transform, background_color_mesh))
+            .id();
 
         debug!("Processing LevelBackgroundColor for {entity:?}");
 
